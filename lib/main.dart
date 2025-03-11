e2ba0c8ce47ed641db9d4fd041cd8db09b35c8d5
CITLALI_AILEEN_MISCLES_RUVALCABA <a.22308051281264@cbtis128.edu.mx>
2025-03-11 15:42:28 +0000

    Subir de nuevo

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,119 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor:
+              Colors.green, // Fondo verde para la barra de navegación
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center, // Centrar los íconos
+            children: [
+              IconButton(
+                  icon: Icon(Icons.home, color: Colors.white),
+                  onPressed: () {}),
+              IconButton(
+                  icon: Icon(Icons.favorite, color: Colors.white),
+                  onPressed: () {}),
+              IconButton(
+                  icon: Icon(Icons.notifications, color: Colors.white),
+                  onPressed: () {}),
+              IconButton(
+                  icon: Icon(Icons.message, color: Colors.white),
+                  onPressed: () {}),
+            ],
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search, color: Colors.white),
+                onPressed: () {}),
+            IconButton(
+                icon: Icon(Icons.more_vert, color: Colors.white),
+                onPressed: () {}),
+          ],
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://raw.githubusercontent.com/Ailin-Miscles-5J/p8_Imagenes/refs/heads/master/65f9d492598ea.r_d.1079-279-5658.jpeg'),
+              ),
+              title: Text('Javaid Durant'),
+              subtitle: Text('Programmer'),
+              trailing: OvalButton(),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://raw.githubusercontent.com/Ailin-Miscles-5J/p8_Imagenes/refs/heads/master/65f9d492598ea.r_d.1079-279-5658.jpeg'),
+              ),
+              title: Text('Ahmed Khan'),
+              subtitle: Text('Data Summit'),
+              trailing: OvalButton(),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://raw.githubusercontent.com/Ailin-Miscles-5J/p8_Imagenes/refs/heads/master/65f9d492598ea.r_d.1079-279-5658.jpeg'),
+              ),
+              title: Text('Aqib'),
+              subtitle: Text('Project Manager'),
+              trailing: OvalButton(),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://raw.githubusercontent.com/Ailin-Miscles-5J/p8_Imagenes/refs/heads/master/65f9d492598ea.r_d.1079-279-5658.jpeg'),
+              ),
+              title: Text('Zaid'),
+              subtitle: Text('Terra Center'),
+              trailing: OvalButton(),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://raw.githubusercontent.com/Ailin-Miscles-5J/p8_Imagenes/refs/heads/master/65f9d492598ea.r_d.1079-279-5658.jpeg'),
+              ),
+              title: Text('Saqib'),
+              subtitle: Text('Developer'),
+              trailing: OvalButton(),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          // Fondo verde para la barra inferior
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Citali Miscles 1264',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class OvalButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(20.0),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      child: Text('View', style: TextStyle(color: Colors.white)),
     );
   }
 }
